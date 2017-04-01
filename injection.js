cdc47e000e70c1b667b5afc231883a26629c5efd
Nikolay Chistyakov <nikolay@chistyakov.me>
2017-04-02 01:34:37 +0300

    1

---
+++ injection.js
@@ -0,0 +1,10 @@
+// var script = document.createElement('script');
+// script.innerHTML = 'window.user_permissions = ["admin"];';
+// script.innerHTML+='window.user_pro_member = true;';
+// script.innerHTML+='window.user_has_pro_permissions = true;';
+
+// document.getElementsByTagName('head')[0].appendChild(script);
+
+window.user_permissions = [];
+window.user_pro_member = false;
+window.user_has_pro_permissions = false
